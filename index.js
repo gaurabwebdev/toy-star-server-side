f871626a1e5a07f428a971b4043fe931d9cc489e
gaurabwebdev <gaurabbose40@gmail.com>
2023-05-19 18:55:10 +0600

    mongodb database connected

--- index.js
+++ index.js
@@ -3,10 +3,40 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Welcome To Khelaghor Server");
+const uri = `mongodb+srv://${process.env.mongoDB_USER}:${process.env.mongoDB_PASS}@cluster0.wqlyhsd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome To Khelaghor Server");
+// });
 
 app.listen(port, () => {
   console.log(`Khelaghor is running on ${port}`);
